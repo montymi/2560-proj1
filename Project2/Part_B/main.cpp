5e9680867e59c3f681053f9aa9ed85365d39ce22
montymi <mcmontanaro01@gmail.com>
2023-10-30 20:48:36 -0400

    apologies for the changes to main again, just finishing the logging cleanup

--- Project2/Part_B/main.cpp
+++ Project2/Part_B/main.cpp
@@ -3,17 +3,15 @@
 
 int main()
 {
-  Deck* deck = new Deck();
+  Deck* deck;
   std::cout << *deck;
 
   for (int i = 0; i < 3; i++)
   {
     std::cout << "Loop: " << i << std::endl;
     deck->shuffle();
-    std::cout << "Successfully shuffled\n";
     std::cout  << "\n------ shuffle " << i + 1 << " ----\n\n" << *deck;
   }
-  delete deck;
   return 0;
 }
 
