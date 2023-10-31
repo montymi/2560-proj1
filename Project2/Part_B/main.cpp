06f0f5e2211be36a79b9c289a2a184542e55bbcd
montymi <mcmontanaro01@gmail.com>
2023-10-30 20:51:20 -0400

    adding new and delete back to main

--- Project2/Part_B/main.cpp
+++ Project2/Part_B/main.cpp
@@ -3,15 +3,15 @@
 
 int main()
 {
-  Deck* deck;
+  Deck* deck = new Deck();
   std::cout << *deck;
 
   for (int i = 0; i < 3; i++)
   {
-    std::cout << "Loop: " << i << std::endl;
     deck->shuffle();
     std::cout  << "\n------ shuffle " << i + 1 << " ----\n\n" << *deck;
   }
+  delete deck;
   return 0;
 }
 
