c45a857ba45dd8e5d6357ca6fd286c58afdc7203
montymi <mcmontanaro01@gmail.com>
2023-10-31 23:36:01 -0400

    handling input for playFlip

--- Project2/Part_B/main.cpp
+++ Project2/Part_B/main.cpp
@@ -3,14 +3,27 @@
 
 int main()
 {
-  Deck* deck = new Deck();
-  std::cout << *deck;
+  Deck* deck = new Deck();  // TODO: compare against Deck deck = Deck()
+  std::cout << *deck;       // which makes this << deck
 
   for (int i = 0; i < 3; i++)
   {
-    deck->shuffle();
-    std::cout  << "\n------ shuffle " << i + 1 << " ----\n\n" << *deck;
+    deck->shuffle();      // and this deck.shuffle()
+    std::cout  << "\n------ shuffle " << i + 1 << " ----\n\n" << *deck; // and this << deck
   }
-  delete deck;
+  delete deck;          // this would be removed
   return 0;
+}
+
+void playFlip()
+{
+  std::string userInput;
+  std::cout << "Flip (Y/n): ";
+  std::cin >> userInput;
+
+  if (userInput.empty() || userInput == "Y" || userInput == "y") {
+      std::cout << "Flipping..." << std::endl;
+  } else {
+      std::cout << "Exiting." << std::endl;
+  }
 }
