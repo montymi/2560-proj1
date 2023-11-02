135edaa6dcc85c38786c0a64bd9128b45cbd1480
montymi <mcmontanaro01@gmail.com>
2023-11-02 14:38:14 -0400

    updating gitignore and adding initial logic for playFlip()

--- Project2/Part_B/main.cpp
+++ Project2/Part_B/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "Deck.h"
 
+void playFlip(Deck& deck);
+
 int main()
 {
   Deck* deck = new Deck();  // TODO: compare against Deck deck = Deck()
@@ -11,19 +13,37 @@ int main()
     deck->shuffle();      // and this deck.shuffle()
     std::cout  << "\n------ shuffle " << i + 1 << " ----\n\n" << *deck; // and this << deck
   }
+  playFlip(*deck);
   delete deck;          // this would be removed
   return 0;
 }
 
-void playFlip()
+void playFlip(Deck& deck)
 {
-  std::string userInput;
-  std::cout << "Flip (Y/n): ";
-  std::cin >> userInput;
-
-  if (userInput.empty() || userInput == "Y" || userInput == "y") {
-      std::cout << "Flipping..." << std::endl;
-  } else {
-      std::cout << "Exiting." << std::endl;
+  bool play = true;
+  std::cout << "\nTop 24 cards:\n";
+  Card* currentCard = deck.deal();
+  std::vector<Card*> top24Cards = {currentCard};
+  for (int i = 0; i < 24; i++) {
+      currentCard = deck.deal();
+      top24Cards.push_back(currentCard);
+      std::cout << i << "\t| " << *currentCard;
+  }
+  std::cout << "\nRemaining cards in the deck:\n";
+  std::cout << deck;       // which makes this << deck
+  std::string action;
+  while (play) {
+     std::cout << "\nDo you want to flip the next card? (Type 'flip' or 'quit'): ";
+     std::cin >> action;
+     if (action == "Y" || action == "y" || action.empty()) {
+       std::cout << "Continuing...\n";
+     }
+     else if (action == "N" || action == "n") {
+       std::cout << "Current Score: ";
+       play = false;
+     }
+     else {
+       std::cout << "Please input a y/Y to flip or n/N to end\n";
+     }
   }
 }
