39812fa36616dd96dfb256ada821fdc865981dd1
William-Spector <113060480+William-Spector@users.noreply.github.com>
2023-11-09 11:34:22 -0500

    make template for part 3

---
+++ Project3/Part_A/Dictionary.cpp
@@ -0,0 +1,27 @@
+#include "Dictionary.h"
+#include <iostream>
+
+Dictionary::Dictionary(void)
+{
+  (void) wordlist;
+}
+
+void Dictionary::readWords(std::string filename)
+{
+  (void) filename;
+}
+
+void Dictionary::printWords(void)
+{
+  
+}
+
+void Dictionary::sortWords(void)
+{
+  
+}
+
+void Dictionary::lookupWord(std::string word)
+{
+  (void) word;
+}
---
+++ Project3/Part_A/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Dictionary.h"
+#include "Grid.h"
+
+void findMatches(Dictionary dict, Grid grid);
+
+int main(){
+  return 0;
+}
+
+void findMatches(Dictionary dict, Grid grid)
+{
+  (void) dict;
+  (void) grid;
+}
