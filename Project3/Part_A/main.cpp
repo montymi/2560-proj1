0dfe98e1c4f294d38e9f21427a55a5d7bd82e491
montymi <mcmontanaro01@gmail.com>
2023-11-19 13:14:35 -0500

    wip

--- Project3/Part_A/main.cpp
+++ Project3/Part_A/main.cpp
@@ -3,10 +3,10 @@
 #include "Dictionary.h"
 #include "Grid.h"
 
-#define MIN_LENGTH = 5
+const int MIN_LENGTH = 5;
 
 void findMatches(Dictionary dict, Grid grid);
-void search(Grid grid, int row, int col);
+void search(Grid grid, int row, int col, std::vector<std::string> found);
 
 int main(){
   return 0;
@@ -14,38 +14,36 @@ int main(){
 
 void findMatches(Dictionary dict, Grid grid)
 {
-  Dictionary dict = Dictionary();
-  (void) grid;
   int width = grid.getWidth(), height = grid.getHeight();
   for (int row = 0; row < height; row++) {
     for (int col = 0; col < width; col++) {
       std::vector<std::string> found;
       search(grid, row, col, found);
 
-      for (const string &match : found) {
-        if dict.lookupWord(match) {std::cout << match << std::endl;}
+      for (const std::string &match : found) {
+        if (dict.lookupWord(match)) {std::cout << match << std::endl;}
       }
     }
   }
 }
 
-void search(Grid grid, int row, int col, std::vector found)
+void search(Grid grid, int row, int col, std::vector<std::string> found)
 {
   int x[] = {};
   int y[] = {};
 
   for (int direction = 0; direction < 8; direction++) {
-    string current = "";
-    int rd = row + x[dir];
-    int cd = col + y[dir];
+    std::string current = "";
+    int rd = row + x[direction];
+    int cd = col + y[direction];
 
     while (rd > -1 && rd < grid.getHeight() && cd > -1 && grid.getWidth()) {
-      current += grid[rd][cd];
+      current += grid.getLetter(cd, rd);
       if (current.length() >= MIN_LENGTH) {
         found.push_back(current);
       }
-      rd += x[dir];
-      cd += y[dir];
+      rd += x[direction];
+      cd += y[direction];
     }
   }
 }
