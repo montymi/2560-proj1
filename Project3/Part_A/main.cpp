6890e9fac61c8c09533db2dbccce421759d35f44
Michael Montanaro <mcmontanaro01@gmail.com>
2023-11-20 18:15:57 -0500

    adding wrapper

--- Project3/Part_A/main.cpp
+++ Project3/Part_A/main.cpp
@@ -12,6 +12,7 @@ int main(){
   Dictionary dict = Dictionary();
   Grid grid = Grid("input15.txt");
   dict.readWords("dictionary.txt");
+  dict.sortWords();
   findMatches(dict, grid);
   return 0;
 }
@@ -28,7 +29,6 @@ void findMatches(Dictionary dict, Grid grid)
 
 void searchDirections(Dictionary dict, Grid grid, int row, int col)
 {
-  dict.sortWords();
   // directional arrays
   int x[] = {-1, -1, -1, 0, 0, 1, 1, 1};
   int y[] = {-1, 0, 1, -1, 1, -1, 0, 1};
@@ -39,7 +39,33 @@ void searchDirections(Dictionary dict, Grid grid, int row, int col)
     int cd = col;
 
     while (rd > -1 && rd < grid.getHeight() && cd > -1 && cd < grid.getWidth()) {
-      current += grid.getLetter(cd, rd);
+      if (rd > -1 && rd < grid.getHeight() && cd > -1 && cd < grid.getWidth()) {
+        current += grid.getLetter(cd, rd);
+      // overflow left
+      } else if (rd < 0 && cd > -1 && cd < grid.getWidth()) {
+        current +=grid.getLetter(cd, grid.getHeight()+rd);
+      // overflow right
+      } else if (rd >= grid.getHeight() && cd > -1 && cd < grid.getWidth()) {
+        current +=grid.getLetter(cd, rd-grid.getHeight());
+      // overflow diag top left
+      } else if (rd < 0 && cd < 0) {
+        current +=grid.getLetter(grid.getWidth()+cd, grid.getHeight()+rd);
+      // overflow diag top right
+      } else if (rd >= grid.getHeight() && cd < 0) {
+        current +=grid.getLetter(grid.getWidth()+cd, rd-grid.getHeight());
+      // overflow diag bottom left
+      } else if (rd < 0 && cd >= grid.getWidth()) {
+        current +=grid.getLetter(cd-grid.getWidth(), grid.getHeight()+rd);
+      // overflow diag bottom right
+      } else if (rd >= grid.getHeight() && cd >= grid.getWidth()) {
+        current +=grid.getLetter(cd-grid.getWidth(), rd-grid.getHeight());
+      // overflow top
+      } else if (rd > -1 && rd < grid.getHeight() && cd < 0) {
+        current +=grid.getLetter(grid.getWidth()+cd, rd);
+      // overflow top
+      } else if (rd > -1 && rd < grid.getHeight() && cd >= grid.getWidth()) {
+        current +=grid.getLetter(cd-grid.getWidth(), rd);
+      }
       if (current.length() >= MIN_LENGTH) {
         if (dict.lookupWord(current) != -1) {std::cout << "MATCH: ";}
         std::cout << current << std::endl;
